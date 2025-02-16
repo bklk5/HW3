968b8fd68267f404f2e4d3c95c38be851c20a490
Andy Le <4ndyle@gmail.com>
2025-02-16 16:09:58 -0700

    fix indentation and add questioin/answer table

--- src/databasePart1/DatabaseHelper.java
+++ src/databasePart1/DatabaseHelper.java
@@ -84,6 +84,26 @@ public class DatabaseHelper {
 	            	+ "isUsed BOOLEAN DEFAULT FALSE,"
 	            	+ "isForgotten BOOLEAN DEFAULT FALSE)";
 	    statement.execute(onetimePasswordTable);
+	    
+	    // Table for answer and questions
+	    String questionsTable = "CREATE TABLE IF NOT EXISTS Questions (" 
+	    		+ "id INT AUTO_INCREMENT PRIMARY KEY, "
+	    		+ "title VARCHAR(255) NOT NULL, "
+	    		+ "content TEXT NOT NULL, " 
+	    		+ "author VARCHAR(100) NOT NULL, " 
+	    		+ "category VARCHAR(64) NOT NULL, " 
+	    		+ "resolved BOOLEAN DEFAULT FALSE, " 
+	    		+ "upvotes INT DEFAULT 0)";
+	    statement.execute(questionsTable);
+	    
+	    String answersTable = "CREATE TABLE IF NOT EXISTS Answers ("
+	    		+ "id INT AUTO_INCREMENT PRIMARY KEY, "
+	    		+ "question_id INT NOT NULL, "
+	    		+ "author VARCHAR(100) NOT NULL, " 
+	    		+ "content TEXT NOT NULL, " 
+	    		+ "upvotes INT DEFAULT 0, "
+	    		+ "FOREIGN KEY (question_id) REFERENCES Questions(id) ON DELETE CASCADE);";
+	    statement.execute(answersTable); 
 	}
 
 
@@ -178,108 +198,108 @@ public class DatabaseHelper {
 	}
 	
 	// Check if the user is a student 
-		public boolean isUserStudent(String userName) {
-		    String query = "SELECT studentRole FROM cse360users WHERE userName = ?";
-		    try (PreparedStatement pstmt = connection.prepareStatement(query)) {
-		        pstmt.setString(1, userName);
-		        ResultSet rs = pstmt.executeQuery();
-		        
-		        if (rs.next()) {
-		            return rs.getBoolean("studentRole"); // Return the role if user exists
-		        }
-		    } catch (SQLException e) {
-		        e.printStackTrace();
-		    }
-		    return false; // User does not have the student role
+	public boolean isUserStudent(String userName) {
+	    String query = "SELECT studentRole FROM cse360users WHERE userName = ?";
+	    try (PreparedStatement pstmt = connection.prepareStatement(query)) {
+	        pstmt.setString(1, userName);
+	        ResultSet rs = pstmt.executeQuery();
+	        
+	        if (rs.next()) {
+	            return rs.getBoolean("studentRole"); // Return the role if user exists
+	        }
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+	    return false; // User does not have the student role
+	}
+	// Check if the user is a instructor 
+	public boolean isUserInstructor(String userName) {
+		String query = "SELECT instructorRole FROM cse360users WHERE userName = ?";
+		try (PreparedStatement pstmt = connection.prepareStatement(query)) {
+				pstmt.setString(1, userName);
+			    ResultSet rs = pstmt.executeQuery();
+			        
+			    if (rs.next()) {
+			    	return rs.getBoolean("instructorRole"); // Return the role if user exists
+			    }
+			  } catch (SQLException e) {
+			      e.printStackTrace();
+			  }
+			  return false; // User does not have the instructor role
 		}
-		// Check if the user is a instructor 
-		public boolean isUserInstructor(String userName) {
-			String query = "SELECT instructorRole FROM cse360users WHERE userName = ?";
-			try (PreparedStatement pstmt = connection.prepareStatement(query)) {
-					pstmt.setString(1, userName);
-				    ResultSet rs = pstmt.executeQuery();
-				        
-				    if (rs.next()) {
-				    	return rs.getBoolean("instructorRole"); // Return the role if user exists
-				    }
-				  } catch (SQLException e) {
-				      e.printStackTrace();
-				  }
-				  return false; // User does not have the instructor role
-			}
-		// Check if the user is a staff
-		public boolean isUserStaff(String userName) {
-			String query = "SELECT staffRole FROM cse360users WHERE userName = ?";
-			try (PreparedStatement pstmt = connection.prepareStatement(query)) {
-					pstmt.setString(1, userName);
-				    ResultSet rs = pstmt.executeQuery();
-				        
-				    if (rs.next()) {
-				    	return rs.getBoolean("staffRole"); // Return the role if user exists
-				    }
-				  } catch (SQLException e) {
-				      e.printStackTrace();
-				  }
-				  return false; // User does not have the staff role
-			}
-		public boolean isUserReviewer(String userName) {
-			String query = "SELECT reviewerRole FROM cse360users WHERE userName = ?";
-			try (PreparedStatement pstmt = connection.prepareStatement(query)) {
-					pstmt.setString(1, userName);
-				    ResultSet rs = pstmt.executeQuery();
-				        
-				    if (rs.next()) {
-				    	return rs.getBoolean("reviewerRole"); // Return the role if user exists
-				    }
-				  } catch (SQLException e) {
-				      e.printStackTrace();
-				  }
-				  return false; // User does not have the reviewer role
-			}
+	// Check if the user is a staff
+	public boolean isUserStaff(String userName) {
+		String query = "SELECT staffRole FROM cse360users WHERE userName = ?";
+		try (PreparedStatement pstmt = connection.prepareStatement(query)) {
+				pstmt.setString(1, userName);
+			    ResultSet rs = pstmt.executeQuery();
+			        
+			    if (rs.next()) {
+			    	return rs.getBoolean("staffRole"); // Return the role if user exists
+			    }
+			  } catch (SQLException e) {
+			      e.printStackTrace();
+			  }
+			  return false; // User does not have the staff role
+		}
+	public boolean isUserReviewer(String userName) {
+		String query = "SELECT reviewerRole FROM cse360users WHERE userName = ?";
+		try (PreparedStatement pstmt = connection.prepareStatement(query)) {
+				pstmt.setString(1, userName);
+			    ResultSet rs = pstmt.executeQuery();
+			        
+			    if (rs.next()) {
+			    	return rs.getBoolean("reviewerRole"); // Return the role if user exists
+			    }
+			  } catch (SQLException e) {
+			      e.printStackTrace();
+			  }
+			  return false; // User does not have the reviewer role
+		}
+
 	
+	// initial roles set when creating invitation code
+	public boolean[] initialRoles(String code) {
+	    String query = "SELECT * FROM InvitationCodes WHERE code = ?";
+	    boolean[] result = {false,false,false,false,false};
+	    try (PreparedStatement pstmt = connection.prepareStatement(query)) {
+	        
+	        pstmt.setString(1, code);
+	        ResultSet rs = pstmt.executeQuery();
+	        
+	        if (rs.next()) {
+	            result[0] = rs.getBoolean("adminRole");
+	            result[1] = rs.getBoolean("studentRole");
+	            result[2] = rs.getBoolean("instructorRole");
+	            result[3] = rs.getBoolean("staffRole");
+	            result[4] = rs.getBoolean("reviewerRole");
+	            
+	        }
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+	    return result; //return the roles that are assigned to the invitation code
+	}		
+	
+	
+	
+	// return the amount of role that the user has been assigned
+	public int numberOfRoles(String userName) {
+		int total = 0;
+		if(isUserAdmin(userName))
+			total+=1;
+		if(isUserStudent(userName))
+			total+=1;
+		if(isUserInstructor(userName))
+			total+=1;
+		if(isUserStaff(userName))
+			total+=1;
+		if(isUserReviewer(userName))
+			total+=1;
 		
-		// initial roles set when creating invitation code
-		public boolean[] initialRoles(String code) {
-		    String query = "SELECT * FROM InvitationCodes WHERE code = ?";
-		    boolean[] result = {false,false,false,false,false};
-		    try (PreparedStatement pstmt = connection.prepareStatement(query)) {
-		        
-		        pstmt.setString(1, code);
-		        ResultSet rs = pstmt.executeQuery();
-		        
-		        if (rs.next()) {
-		            result[0] = rs.getBoolean("adminRole");
-		            result[1] = rs.getBoolean("studentRole");
-		            result[2] = rs.getBoolean("instructorRole");
-		            result[3] = rs.getBoolean("staffRole");
-		            result[4] = rs.getBoolean("reviewerRole");
-		            
-		        }
-		    } catch (SQLException e) {
-		        e.printStackTrace();
-		    }
-		    return result; //return the roles that are assigned to the invitation code
-		}		
-		
-		
-		
-		// return the amount of role that the user has been assigned
-		public int numberOfRoles(String userName) {
-			int total = 0;
-			if(isUserAdmin(userName))
-				total+=1;
-			if(isUserStudent(userName))
-				total+=1;
-			if(isUserInstructor(userName))
-				total+=1;
-			if(isUserStaff(userName))
-				total+=1;
-			if(isUserReviewer(userName))
-				total+=1;
-			
-			return total;
-		}
-		
+		return total;
+	}
+	
 	
 	// Generates a new invitation code and inserts it into the database.
 	public String generateInvitationCode(boolean adminRole, boolean studentRole, boolean instructorRole, boolean staffRole, boolean reviewerRole, Timestamp timeInviteMade) {
@@ -425,105 +445,107 @@ public class DatabaseHelper {
 	}
 
 	// Generates a new onetime password and insert it into the database
-		public String generateOnetimePassword(String username) {
-			String password = OnetimePasswordGenerator.random_password();
-			String query = "INSERT INTO OnetimePassword(password, username, isUsed, isForgotten) VALUES (?, ?, ?, ?)";
-			
-			try (PreparedStatement pstmt = connection.prepareStatement(query)) {
-				pstmt.setString(1, password);
-				pstmt.setString(2, username);
-				pstmt.setBoolean(3, false);
-				pstmt.setBoolean(4, false);
-				pstmt.executeUpdate();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			return password;
+	public String generateOnetimePassword(String username) {
+		String password = OnetimePasswordGenerator.random_password();
+		String query = "INSERT INTO OnetimePassword(password, username, isUsed, isForgotten) VALUES (?, ?, ?, ?)";
+		
+		try (PreparedStatement pstmt = connection.prepareStatement(query)) {
+			pstmt.setString(1, password);
+			pstmt.setString(2, username);
+			pstmt.setBoolean(3, false);
+			pstmt.setBoolean(4, false);
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
-		
-		// Validate a onetime password to check if it is unused
-		public boolean validateOnetimePassword(String password, String username) {
-			String query = "SELECT * FROM OnetimePassword WHERE password = ? AND username = ? AND isUsed = FALSE";
-			
-			try (PreparedStatement pstmt = connection.prepareStatement(query)) {
-				pstmt.setString(1, password);
-				pstmt.setString(2, username);
-				ResultSet rs = pstmt.executeQuery();
-				if (rs.next()) {
-					// Mark the password as used
-					markOnetimePasswordAsUsed(password);
-					return true;
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			return false;
-		}
-		
-		// Mark the onetime password as used in the database
-		private void markOnetimePasswordAsUsed(String password) {
-			String query = "UPDATE OnetimePassword SET isUsed = TRUE WHERE password = ?";
-			try (PreparedStatement pstmt = connection.prepareStatement(query)) {
-				pstmt.setString(1, password);
-				pstmt.executeUpdate();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		// change password 
-		public void changePassword(String new_password, String username) {
-			String query = "UPDATE cse360users SET password = ? WHERE userName = ?";
-			
-			try (PreparedStatement pstmt = connection.prepareStatement(query)) {
-				pstmt.setString(1, new_password);
-				pstmt.setString(2, username);
-				pstmt.executeUpdate();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		public String retrieveUsersInfo() {
-			String result = "";
-			String query = "SELECT * FROM cse360users";
-		    try (PreparedStatement pstmt = connection.prepareStatement(query)) {
-		        ResultSet rs = pstmt.executeQuery();
-		        
-		        while(rs.next()) {
-		        	String username = rs.getString("userName");
-		        	result += rs.getString("name") + ",";
-		        	result += rs.getString("email") + ",";
-		        	result += username + ",";
-		        	result += "Roles: ";
-		        	if(isUserAdmin(username))
-		        		result += "admin" + ",";
-		        	if(isUserStudent(username))
-		        		result += "student" + ",";
-		        	if(isUserInstructor(username))
-		        		result += "instructor" + ",";
-		        	if(isUserStaff(username))
-		        		result += "staff" + ",";
-		        	if(isUserReviewer(username))
-		        		result += "reviewer" + ",";
-		        	result += "\n";
-		          
-		        }
-		        return result;
-		    } catch (SQLException e) {
-		        e.printStackTrace();
-		    }
-		    return result; // Database emtpy
-			
-		}
-
-			
-			
-			
-		
-		
-		
-
+		return password;
+	}
 	
+	// Validate a onetime password to check if it is unused
+	public boolean validateOnetimePassword(String password, String username) {
+		String query = "SELECT * FROM OnetimePassword WHERE password = ? AND username = ? AND isUsed = FALSE";
+		
+		try (PreparedStatement pstmt = connection.prepareStatement(query)) {
+			pstmt.setString(1, password);
+			pstmt.setString(2, username);
+			ResultSet rs = pstmt.executeQuery();
+			if (rs.next()) {
+				// Mark the password as used
+				markOnetimePasswordAsUsed(password);
+				return true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+		
+	// Mark the onetime password as used in the database
+	private void markOnetimePasswordAsUsed(String password) {
+		String query = "UPDATE OnetimePassword SET isUsed = TRUE WHERE password = ?";
+		try (PreparedStatement pstmt = connection.prepareStatement(query)) {
+			pstmt.setString(1, password);
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// change password 
+	public void changePassword(String new_password, String username) {
+		String query = "UPDATE cse360users SET password = ? WHERE userName = ?";
+		
+		try (PreparedStatement pstmt = connection.prepareStatement(query)) {
+			pstmt.setString(1, new_password);
+			pstmt.setString(2, username);
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	public String retrieveUsersInfo() {
+		String result = "";
+		String query = "SELECT * FROM cse360users";
+	    try (PreparedStatement pstmt = connection.prepareStatement(query)) {
+	        ResultSet rs = pstmt.executeQuery();
+	        
+	        while(rs.next()) {
+	        	String username = rs.getString("userName");
+	        	result += rs.getString("name") + ",";
+	        	result += rs.getString("email") + ",";
+	        	result += username + ",";
+	        	result += "Roles: ";
+	        	if(isUserAdmin(username))
+	        		result += "admin" + ",";
+	        	if(isUserStudent(username))
+	        		result += "student" + ",";
+	        	if(isUserInstructor(username))
+	        		result += "instructor" + ",";
+	        	if(isUserStaff(username))
+	        		result += "staff" + ",";
+	        	if(isUserReviewer(username))
+	        		result += "reviewer" + ",";
+	        	result += "\n";
+	          
+	        }
+	        return result;
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+	    return result; // Database emtpy
+		
+	}
+	
+	
+
+		
+		
+		
+	
+	
+	
+
+
 	// Closes the database connection and statement.
 	public void closeConnection() {
 		try{ 
@@ -537,5 +559,7 @@ public class DatabaseHelper {
 			se.printStackTrace(); 
 		} 
 	}
+
+
 
 }
